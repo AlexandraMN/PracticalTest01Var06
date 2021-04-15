30417cee56695017ebe60208413e617995e4d39b
Nicoleta-Alexandra MĂRĂCINE (94703) <nicoleta.maracine@stud.acs.upb.ro>
2021-04-15 12:38:06 +0300

    Added task A

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -3,12 +3,36 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class PracticalTest01Var06MainActivity extends AppCompatActivity {
+
+    EditText mStarEt;
+    EditText mOneEt;
+    EditText mZeroEt;
+    CheckBox mStarCb;
+    CheckBox mOneCb;
+    CheckBox mZeroCb;
+    Button mPlayBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_main);
+
+        mStarEt = findViewById(R.id.star);
+        mOneEt = findViewById(R.id.one);
+        mZeroEt = findViewById(R.id.zero);
+        mStarCb = findViewById(R.id.checkbox_star);
+        mOneCb = findViewById(R.id.checkbox_one);
+        mZeroCb = findViewById(R.id.checkbox_zero);
+        mPlayBtn = findViewById(R.id.play_button);
+
+        mStarEt.setText("*");
+        mZeroEt.setText("0");
+        mOneEt.setText("1");
+
     }
 }
